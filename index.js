8a16310340f920d099bf670e76fca98c44c5573f
小艾 <103049662+liuaiying0704@users.noreply.github.com>
2022-05-11 21:39:21 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,129 +1,94 @@
-window.addEventListener('load', function() {
-    // 1. 获取元素
-    var arrow_l = document.querySelector('.arrow-l');
-    var arrow_r = document.querySelector('.arrow-r');
-    var focus = document.querySelector('.focus');
-    var focusWidth = focus.offsetWidth;
-    // 2. 鼠标经过focus 就显示隐藏左右按钮
-    focus.addEventListener('mouseenter', function() {
-        arrow_l.style.display = 'block';
-        arrow_r.style.display = 'block';
-        clearInterval(timer);
-        timer = null; // 清除定时器变量
-    });
-    focus.addEventListener('mouseleave', function() {
-        arrow_l.style.display = 'none';
-        arrow_r.style.display = 'none';
-        timer = setInterval(function() {
-            //手动调用点击事件
-            arrow_r.click();
-        }, 2000);
-    });
-    // 3. 动态生成小圆圈  有几张图片，我就生成几个小圆圈
-    var ul = focus.querySelector('ul');
-    var ol = focus.querySelector('.circle');
-    // console.log(ul.children.length);
-    for (var i = 0; i < ul.children.length; i++) {
-        // 创建一个小li 
-        var li = document.createElement('li');
-        // 记录当前小圆圈的索引号 通过自定义属性来做 
-        li.setAttribute('index', i);
-        // 把小li插入到ol 里面
-        ol.appendChild(li);
-        // 4. 小圆圈的排他思想 我们可以直接在生成小圆圈的同时直接绑定点击事件
-        li.addEventListener('click', function() {
-            // 干掉所有人 把所有的小li 清除 current 类名
-            for (var i = 0; i < ol.children.length; i++) {
-                ol.children[i].className = '';
-            }
-            // 留下我自己  当前的小li 设置current 类名
-            this.className = 'current';
-            // 5. 点击小圆圈，移动图片 当然移动的是 ul 
-            // ul 的移动距离 小圆圈的索引号 乘以 图片的宽度 注意是负值
-            // 当我们点击了某个小li 就拿到当前小li 的索引号
-            var index = this.getAttribute('index');
-            // 当我们点击了某个小li 就要把这个li 的索引号给 num  
-            num = index;
-            // 当我们点击了某个小li 就要把这个li 的索引号给 circle  
-            circle = index;
-            // num = circle = index;
-            console.log(focusWidth);
-            console.log(index);
+window.addEventListener('load', function () {
+  var num = 0;
+  var focus = document.querySelector('.focus');
+  var ul = focus.children[0];
+  var ol = focus.children[1];
+  var w = focus.offsetWidth;
 
-            animate(ul, -index * focusWidth);
-        })
+  //  1、定时器：图片自动滚动
+  var timer = setInterval(function (e) {
+    num++;
+    var translatex = -num * w;
+    ul.style.transition = 'all .3s';
+    ul.style.transform = 'translateX(' + translatex + 'px)';
+  }, 2000);
+
+  //  2、无缝轮播，transitionend检测过渡完成之后再去判断。最后一张图索引是3
+  ul.addEventListener('transitionend', function (e) {
+    if (num >= 3) {
+      num = 0;
+      ul.style.transition = 'none';
+      var translatex = -num * w;
+      ul.style.transform = 'translateX(' + translatex + 'px)';
+    } else if (num < 0) {
+      num = 2;
+      ul.style.transition = 'none';
+      var translatex = -num * w;
+      ul.style.transform = 'translateX(' + translatex + 'px)';
     }
-    // 把ol里面的第一个小li设置类名为 current
-    ol.children[0].className = 'current';
-    // 6. 克隆第一张图片(li)放到ul 最后面
-    var first = ul.children[0].cloneNode(true);
-    ul.appendChild(first);
-    // 7. 点击右侧按钮， 图片滚动一张
-    var num = 0;
-    // circle 控制小圆圈的播放
-    var circle = 0;
-    // flag 节流阀
-    var flag = true;
-    arrow_r.addEventListener('click', function() {
-        if (flag) {
-            flag = false; // 关闭节流阀
-            // 如果走到了最后复制的一张图片，此时 我们的ul 要快速复原 left 改为 0
-            if (num == ul.children.length - 1) {
-                ul.style.left = 0;
-                num = 0;
-            }
-            num++;
-            animate(ul, -num * focusWidth, function() {
-                flag = true; // 打开节流阀
-            });
-            // 8. 点击右侧按钮，小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
-            circle++;
-            // 如果circle == 4 说明走到最后我们克隆的这张图片了 我们就复原
-            if (circle == ol.children.length) {
-                circle = 0;
-            }
-            // 调用函数
-            circleChange();
-        }
-    });
 
-    // 9. 左侧按钮做法
-    arrow_l.addEventListener('click', function() {
-        if (flag) {
-            flag = false;
-            if (num == 0) {
-                num = ul.children.length - 1;
-                ul.style.left = -num * focusWidth + 'px';
+    // 3、小圆点跟着图片，classList，排他
+    // 找到有current类名的去除
+    ol.querySelector('.current').classList.remove('current');
+    // num可以表示当前播放的图片对应的小圆点的索引。
+    ol.children[num].classList.add('current');
+  });
 
-            }
-            num--;
-            animate(ul, -num * focusWidth, function() {
-                flag = true;
-            });
-            // 点击左侧按钮，小圆圈跟随一起变化 可以再声明一个变量控制小圆圈的播放
-            circle--;
-            // 如果circle < 0  说明第一张图片，则小圆圈要改为第4个小圆圈（3）
-            // if (circle < 0) {
-            //     circle = ol.children.length - 1;
-            // }
-            circle = circle < 0 ? ol.children.length - 1 : circle;
-            // 调用函数
-            circleChange();
-        }
-    });
+  //   4、拖动图片
+  //
+  var startX = 0;
+  var moveX = 0;
+  var flag = false;
+  ul.addEventListener('touchstart', function (e) {
+    startX = e.targetTouches[0].pageX;
+    clearInterval(timer);
+  });
+  ul.addEventListener('touchmove', function (e) {
+    //   移动的距离
+    moveX = e.targetTouches[0].pageX - startX;
+    var translatex = -num * w + moveX;
+    ul.style.transition = 'none';
+    ul.style.transform = 'translateX(' + translatex + 'px)';
+    flag = true;
+    e.preventDefault();
+  });
 
-    function circleChange() {
-        // 先清除其余小圆圈的current类名
-        for (var i = 0; i < ol.children.length; i++) {
-            ol.children[i].className = '';
+  ul.addEventListener('touchend', function (e) {
+    if (flag) {
+      if (Math.abs(moveX) > 50) {
+        if (moveX > 0) {
+          num--;
+        } else {
+          num++;
         }
-        // 留下当前的小圆圈的current类名
-        ol.children[circle].className = 'current';
+        var translatex = -num * w;
+        ul.style.transition = 'all 0.3s';
+        ul.style.transform = 'translateX(' + translatex + 'px)';
+      } else {
+        var translatex = -num * w;
+        ul.style.transition = 'all 0.1s';
+        ul.style.transform = 'translateX(' + translatex + 'px)';
+      }
     }
-    // 10. 自动播放轮播图
-    var timer = setInterval(function() {
-        //手动调用点击事件
-        arrow_r.click();
+    clearInterval(timer);
+    timer = setInterval(function (e) {
+      num++;
+      var translatex = -num * w;
+      ul.style.transition = 'all 0.3s';
+      ul.style.transform = 'translateX(' + translatex + 'px)';
     }, 2000);
-
-})
+  });
+  var nav = document.querySelector('nav');
+  var navTop = nav.offsetTop;
+  var goBack = document.querySelector('.goBack');
+  window.addEventListener('scroll', function (e) {
+    if (window.pageYOffset >= navTop) {
+      goBack.style.display = 'block';
+    } else {
+      goBack.style.display = 'none';
+    }
+  });
+  goBack.addEventListener('click', function () {
+    window.scroll(0, 0);
+  });
+});
